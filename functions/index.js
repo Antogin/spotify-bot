cc88c95d84e4170de3578594a5c0cb017e8563b3
antogin <georges.indrianjafy@gmail.com>
2019-05-09 09:16:14 +0200

    feat: handle song request

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-admin.initializeApp();
+const axios = require('axios');
 
 exports.fulfilment = functions.https.onRequest((req, res) => {
   const { SongName } = req.body.queryResult.parameters;
-  res.json({ fulfillmentText: `Song Name: ${SongName}` });
+
+  const authBody = {
+    grant_type: 'client_credentials',
+  };
+  const authHeaders = {
+    Authorization: `Basic ${process.env.SPOTIFY_TOKEN}`,
+  };
+
+
+  axios.post('https://accounts.spotify.com/api/token', authBody, { headers: authHeaders })
+    .then(({ data }) => {
+      const accessToken = data.access_token;
+
+      const headers = { Authorization: `Bearer ${accessToken}` };
+      const params = { q: SongName, type: 'track' };
+
+      return axios.get('https://api.spotify.com/v1/search', { headers, params })
+        .then(({ data: { tracks } }) => {
+          const { track } = tracks.find(item => !!item.track.preview_url);
+
+          res.json({ fulfillmentText: `Here is the song: ${track.preview_url}` });
+        })
+        .catch((err) => {
+          res.json({ fulfillmentText: `Sorry, could not find this song ${err}` });
+        });
+    }).catch((err) => {
+      console.error('err', err);
+      res.json({ fulfillmentText: `Sorry, could not find this song ${err}` });
+    });
+  // res.json({ fulfillmentText: `Song Name: ${SongName}` });
 });
